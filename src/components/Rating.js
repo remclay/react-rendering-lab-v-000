c27b0c81401395987a995ee99df79886a953aa6e
remclay <remmy.lauren@gmail.com>
2018-06-14 19:04:17 +1000

    Done.

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -11,6 +11,13 @@ class Rating extends React.Component {
     };
   }
 
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      increasing: nextProps.rating > this.props.rating ? true : false,
+      decreasing: nextProps.rating < this.props.rating ? true : false
+    });
+  }
+
   render() {
     let trend = 'stable';
     if (this.state.increasing) {
